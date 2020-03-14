eaf896d7baac420fbe0e778000cf005beedaa0c1
annmweru <annmweru9@gmail.com>
2020-03-14 12:30:54 -0700

    marks

---
+++ marks/src/marks/Marks.java
@@ -0,0 +1,18 @@
+
+package marks;
+import java.util.Scanner;
+public class Marks {
+    public static void main(String[] args) {
+       Scanner Scan=new Scanner(System.in);
+       System.out.println("enter the marks");
+       int marks =scan.nextInt();
+       if((marks>=80)&&(marks<=100)){
+         System.out.println("well done");}
+         if ((marks>=60)&&(marks<=79)){
+             
+          System.out.println("improve");   
+       
+    }
+     }
+    
+}
